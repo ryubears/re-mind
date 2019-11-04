23e246e4f552fb19cd0a6e0dccc2ab3710ab254c
ryubears <ryubears@gmail.com>
2019-11-04 10:09:44 -0600

    Add Firebase Realtime Database

--- app/src/main/java/com/yoseph/re_mind/ui/fragments/OverviewFragment.java
+++ app/src/main/java/com/yoseph/re_mind/ui/fragments/OverviewFragment.java
@@ -9,7 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.yoseph.re_mind.R;
+import com.yoseph.re_mind.data.User;
 
 public class OverviewFragment extends Fragment {
 
@@ -22,6 +25,13 @@ public class OverviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Get a Firebase Realtime Database reference.
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference root = database.getReference();
+
+        User user = new User(1, "Yehyun", "Ryu");
+        root.child("users").child(String.valueOf(user.id)).setValue(user);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_overview, container, false);
     }
