a7639906158e788b4eaea796b88d9459577ab793
Kadin <kadin.schermers@aquinastech.com>
2019-11-04 11:54:56 -0600

    Create Quick Add fragment with prelim design work

---
+++ app/src/main/java/com/yoseph/re_mind/ui/fragments/QuickAddFragment.java
@@ -0,0 +1,19 @@
+package com.yoseph.re_mind.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.yoseph.re_mind.R;
+
+public class QuickAddFragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.quick_add, container, false);
+    }
+}
