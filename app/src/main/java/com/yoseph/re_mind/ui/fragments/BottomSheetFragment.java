9ca860692af1753af5e137415a0e4a2124ed3152
Dongha <dkang0602@gmail.com>
2019-11-10 22:31:02 -0600

    icon changes

--- app/src/main/java/com/yoseph/re_mind/ui/fragments/BottomSheetFragment.java
+++ app/src/main/java/com/yoseph/re_mind/ui/fragments/BottomSheetFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +15,13 @@ import com.yoseph.re_mind.R;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
 
+    private Button addItemButton;
+    private EditText addItemEditText;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Nullable
@@ -23,5 +29,7 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.bottom_sheet, container, false);
     }
+
+
 
 }
